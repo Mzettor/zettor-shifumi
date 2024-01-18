56e2e299b10640f8d203dd863850a4c95df58516
Mzettor <mateozettor@gmail.com>
2024-01-18 22:30:54 +0100

    create repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { css } from '../styled-system/css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className={css({ fontSize: "2xl", fontWeight: 'bold', color: 'red.600'})}>Shifumi Game</h1>
+    </>
+  )
+}
+
+export default App
